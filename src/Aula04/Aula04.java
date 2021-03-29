17ddc5d9275cd3eda10699333712292c435e8f64
kiko <kikopesqueira96@gmail.com>
2021-03-29 17:38:02 -0300

    aula 04

---
+++ src/Aula04/Aula04.java
@@ -0,0 +1,26 @@
+package Aula04;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("Bic",0.5f,"amarela");
+		c1.status();
+		
+		
+		
+		//Caneta ca1 = new Caneta ();
+		//ca1.setModelo("Bic Cristal");
+		//ca1.setPonta(0.7f);
+		//ca1.status();
+
+		//Caneta ca2 = new Caneta();
+		//ca2.setModelo("mundial");
+		//ca2.setPonta(0.5f);
+		//System.out.println( "o modelo da caneta 2 é: "+ ca2.getModelo()+", com ponta de:" + ca2.getPonta()+"!" );
+		
+		
+		
+		
+	}
+
+}
