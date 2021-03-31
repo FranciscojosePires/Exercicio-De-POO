f0a12fc6cea3c2ddafb983d39f0bc298e1097aac
kiko <kikopesqueira96@gmail.com>
2021-03-31 17:09:12 -0300

    aula06

---
+++ src/Aula06/Aula06.java
@@ -0,0 +1,20 @@
+package Aula06;
+
+public class Aula06 {
+
+	public static void main(String[] args) {
+		ControleRemoto c = new ControleRemoto ();
+		c.ligar();
+		c.play();
+		c.maisVolume();
+		c.abrirMenu();
+		//c.desligar();
+		//c.pause();
+		//c.menosVolume();
+		c.fecharMenu();
+		//c.ligarMudo();
+		//c.desligarMudo();
+		
+	}
+
+}
