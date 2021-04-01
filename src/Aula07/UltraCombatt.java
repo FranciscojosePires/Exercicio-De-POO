9a1ef0cd85ee2ad018b0bfaab4598246f1702f1e
kiko <kikopesqueira96@gmail.com>
2021-04-01 17:03:21 -0300

    aula07

---
+++ src/Aula07/UltraCombatt.java
@@ -0,0 +1,31 @@
+package Aula07;
+
+public class UltraCombatt {
+
+	public static void main(String[] args) {
+		lutador l[] = new lutador[6];
+
+		l[0] = new lutador("Pretty Boy", "França", 31, 1.75f,  68.9f, 11, 2, 1); 
+		l[1] = new lutador("Putscript", "Brasil", 29 ,  1.68f,  57.8f, 14, 2, 3);
+		l[2] = new lutador("Snapshadow", "EUA", 35 ,  1.65f,  80.9f, 12, 2, 1);
+		l[3] = new lutador("Dead Code", "Australia",  28,  1.93f,  81.6f, 13, 0, 2);
+		l[4] = new lutador("UFOCollbol", "Brasil", 37 ,  1.70f,  119.3f, 5, 4, 3);
+		l[5] = new lutador("Nerdaart", "EUA", 30 ,  1.81f,  105.7f, 12, 2, 4);
+	
+		l[0].Status();
+		l[1].Status(); 
+		l[2].Status();
+		l[3].Status();
+		l[4].Status();
+		l[5].Status();
+		
+		
+		l[0].apresentar();
+		
+		l[5].ganharLuta();
+		l[5].Status();
+	}
+	
+      
+
+}
---
+++ src/Aula07/lutador.java
@@ -0,0 +1,121 @@
+package Aula07;
+
+public class lutador {
+    //Atributos 
+	private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+    
+    //Metodos publicos 
+    public void apresentar(){
+    	
+    	System.out.println("Apresentamos o lutador "+ this.getNome());
+    	System.out.println("Diretamente de " + this.getNacionalidade());
+    	System.out.println("Com " + this.getIdade() + "anos e " + this.getAltura() + " de altura");
+    	System.out.println("pesando " + this.getPeso() + " kg");
+    	System.out.println(this.getVitorias() + " Vitorias ");
+    	System.out.println(this.getDerrotas() + " Derrotas e ");
+    	System.out.println(this.getEmpates()+" empates!");
+        System.out.println("-------------------------------------------");}
+    public void Status(){
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+    	System.out.println("Gamhou " + this.getVitorias()+ "vezes");
+    	System.out.println("Perdeu " + this.getDerrotas() + "vezes");
+    	System.out.println("Empatou " + this.getEmpates()+ "vezes");
+    	System.out.println("-------------------------------------------------------");
+    }
+    public void ganharLuta(){this.setVitorias(this.getVitorias() + 1);}
+    public void perderLuta(){this.setDerrotas(this.getDerrotas() + 1);}
+    public void empatarLuta(){this.setEmpates(this.getEmpates() + 1);}
+	
+    //metodo /construtor 
+    public lutador(String no, String na, int id, float al, float pe, int vi, int de,int em) {
+		this.nome = no;
+		this.nacionalidade = na;
+		this.idade = id;
+		this.altura = al;
+		this.setPeso(pe);;
+		this.vitorias = vi;
+		this.derrotas = de;
+		this.empates = em;
+	}
+	
+    
+    // Metodos GET and SET
+   
+    public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public float getAltura() {
+		return altura;
+	}
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+         this.peso = peso;
+         this.setCategoria();
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	private void setCategoria() { 
+		if (this.peso < 52.2) {this.categoria="Invalido ";}
+		else if (this.peso <= 70.3) {this.categoria = "Leve";}
+		else if (this.peso <= 83.9) {this.categoria="Medio";}
+		else if (this.peso <= 120.2) {this.categoria="Pesado";}
+		else {this.categoria = "Invalido";}
+		}
+	
+	public int getVitorias() {
+		return vitorias;
+	}
+	public void setVitorias(int vitorias) {
+		this.vitorias = vitorias;
+	}
+	
+	public int getDerrotas() {
+		return derrotas;
+	}
+	public void setDerrotas(int derrotas) {
+		this.derrotas = derrotas;
+	}
+	
+	public int getEmpates() {
+		return empates;
+	}
+	public void setEmpates(int empates) {
+		this.empates = empates;
+	}
+    
+    
+ 
+    
+}
