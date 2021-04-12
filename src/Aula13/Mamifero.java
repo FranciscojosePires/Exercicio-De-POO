f22c44000add3a8ea8065e8a8a3b8a5fe0d1131e
kiko <kikopesqueira96@gmail.com>
2021-04-12 19:44:26 -0300

    aula13

---
+++ src/Aula13/Mamifero.java
@@ -0,0 +1,13 @@
+package Aula13;
+
+public class Mamifero extends Animal {
+	private String corPelo;
+
+	@Override
+	public void emitirSom() {
+		System.out.println("Som de mamífero");
+		
+	}
+	
+
+}
