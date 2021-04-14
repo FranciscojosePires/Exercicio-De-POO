0017f3a2bb16fd0a5521f2a330f49242651b0bb1
kiko <kikopesqueira96@gmail.com>
2021-04-14 02:54:56 -0300

    fim do curro de POO

--- src/ProjetoFinal/ProjetoFinal.java
+++ src/ProjetoFinal/ProjetoFinal.java
@@ -14,13 +14,30 @@ public class ProjetoFinal {
 		System.out.println(v[0].toString());
 		System.out.println(v[1].toString());
 		System.out.println(v[2].toString());
-		
+		System.out.println("\n.......................");
 		
 		a[0] = new Aluno("Roberto ",18,"M ","rob1818");
 		a[1] = new Aluno("Flavia",21,"F ","Flavia@123");
 		
 		System.out.println(a[0].toString());
 		System.out.println(a[1].toString());
+		System.out.println("\n.......................");
+		
+		
+		
+		
+		Visualizacao vis[] = new Visualizacao[5];
+		
+		vis[0] = new Visualizacao(a[0],v[2]);
+		vis[0].avaliar();
+		System.out.println(vis[0].toString());
+		System.out.println(".....................................................");
+		
+		vis[1] = new Visualizacao(a[0],v[1]);
+		vis[0].avaliar(88.0f);
+		System.out.println(vis[1].toString());
+		
+
 		
 
 	}
